9f16132350cad4a8e4d554bcecd8e127ffd0a60d
Nenad Blagov <nbl@nexyo.ag>
2024-10-20 01:09:44 +0200

    contact page background

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import styles from './Contact.module.css'
 
 function Contact() {
     const form = useRef();
@@ -22,24 +23,24 @@ function Contact() {
     };
 
     return (
-        <div className="pt-5 mt-4 d-flex justify-content-center">
+        <div className={styles.contactPageWrapper}>
+            <div className="pt-5 mt-4 d-flex justify-content-center">
 
-            <div className='shadow rounded-large bg-white p-5 w-100' style={{ maxWidth: 750 }}>
-                <h2 className='bold mb-3'>Let's work together!</h2>
-                <p>Feel free to contact me and I'll reply very soon</p>
+                <div className='shadow rounded-large bg-white p-5 w-100' style={{ maxWidth: 750 }}>
+                    <h2 className='bold mb-3'>Let's work together!</h2>
+                    <p>Feel free to contact me and I'll reply very soon</p>
 
-                <form ref={form} onSubmit={sendEmail}>
-                    <label className='mt-4 mb-1 ms-1 fw-500'>Name <span className='text-danger'>*</span> </label>
-                    <input className='form-control' placeholder='Name' type="text" name="user_name" />
-                    <label className='mt-4 mb-1 ms-1 fw-500'>Email <span className='text-danger'>*</span> </label>
-                    <input className='form-control' placeholder='Email' type="email" name="user_email" />
-                    <label className='mt-4 mb-1 ms-1 fw-500'>Message <span className='text-danger'>*</span> </label>
-                    <textarea name="message" className='form-control' placeholder='Write your message...' />
-                    <button className='btn btn-primary rounded w-100 mt-3 bold' style={{padding: '.7rem .6rem'}} type="submit">Submit</button>
-                </form>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <label className='mt-4 mb-1 ms-1 fw-500'>Name <span className='text-danger'>*</span> </label>
+                        <input className='form-control' placeholder='Name' type="text" name="user_name" />
+                        <label className='mt-4 mb-1 ms-1 fw-500'>Email <span className='text-danger'>*</span> </label>
+                        <input className='form-control' placeholder='Email' type="email" name="user_email" />
+                        <label className='mt-4 mb-1 ms-1 fw-500'>Message <span className='text-danger'>*</span> </label>
+                        <textarea name="message" className='form-control' placeholder='Write your message...' />
+                        <button className='btn btn-primary rounded w-100 mt-3 bold' style={{ padding: '.7rem .6rem' }} type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
-
-
         </div>
     );
 };
